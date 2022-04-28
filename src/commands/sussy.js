23a915839398b09cb2af53a36266ea47a4accec1
MarsmiGitHub <68896302+MarsmiGitHub@users.noreply.github.com>
2022-04-28 23:25:04 +0200

    feat: add sussy command (#13)
    * as wise man once said: you cannot create sussy, sussy creates you...
    
    * i hate this man
    
    Co-authored-by: n1ck_pro <59617443+N1ckPro@users.noreply.github.com>
    
    * i hate him even more now
    
    Co-authored-by: n1ck_pro <59617443+N1ckPro@users.noreply.github.com>
    
    Co-authored-by: n1ck_pro <59617443+N1ckPro@users.noreply.github.com>

---
+++ src/commands/sussy.js
@@ -0,0 +1,20 @@
+import { CommandInteraction } from 'discord.js';
+import { Bot } from '../Bot';
+import { Command } from '../Command';
+import { CommandInterface } from '../typings';
+import { CommandType } from '../typings/enums';
+
+export default class SussyCommand extends Command implements CommandInterface {
+    constructor() {
+        super({
+            name: 'sussy',
+            help: 'Sends a sussy gif.',
+            usage: 'sussy',
+            type: CommandType.Misc
+        });
+    }
+
+    execute(client: Bot, interaction: CommandInteraction) {
+        interaction.reply(`https://tenor.com/view/the-rock-rock-gif-21708339`);
+    }
+}
