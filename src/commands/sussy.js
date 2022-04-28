7f91158fdbff0600c41258eda34484c5188ecaf2
MarsmiGitHub <68896302+MarsmiGitHub@users.noreply.github.com>
2022-04-28 23:06:50 +0200

    as wise man once said: you cannot create sussy, sussy creates you...
    10 reasons why add this:
    1. sus
    2. sussy
    3. amogus
    4. sussy
    5. baka
    6. manjot
    7. your comduter has virdus
    8. n1ck is epic and cool unless he doesnt add this
    9. shaun is the best bot unless he doesnt add this
    10. all the previous reasons

---
+++ src/commands/sussy.js
@@ -0,0 +1,20 @@
+import { CommandInteraction } from 'discord.js';
+import { Bot } from '../Bot';
+import { Command } from '../Command';
+import { CommandInterface } from '../typings';
+import { CommandType } from '../typings/enums';
+
+export default class PingCommand extends Command implements CommandInterface {
+    constructor() {
+        super({
+            name: 'sussy',
+            help: 'sends a sussy gif',
+            usage: 'sussy',
+            type: CommandType.Misc
+        });
+    }
+
+    execute(client: Bot, interaction: CommandInteraction) {
+        interaction.reply(`https://tenor.com/view/the-rock-rock-gif-21708339`);
+    }
+}
